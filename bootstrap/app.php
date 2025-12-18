c522438b8e37df95efe5bbfac39657ab4edb5567
hesdin <hesdin01@gmail.com>
2025-12-18 11:31:14 +0800

    Add TrustProxies middleware for HTTPS behind reverse proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies (handle HTTPS behind reverse proxy)
+        $middleware->trustProxies(at: '*');
+
         $middleware->web([
             HandleInertiaRequests::class,
             NoCacheHeaders::class,
